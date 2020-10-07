91cce29c9020d155d6afb3b72449679a51845c30
hech2030 <51271638+hech2030@users.noreply.github.com>
2020-10-07 22:07:01 +0100

    Fix Build

---
+++ MyTeam/Common/Requests/bo/Users/UserDeleteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MyTeam.Common.Requests.bo.Users
+{
+    public class UserDeleteRequest
+    {
+        public string id { get; set; }
+        public string UserName { get; set; }
+        public int UserRole { get; set; }
+    }
+}
